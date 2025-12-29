40512af4708203c9fbffc0987079b68a7c3812fa
Alimoo-ops <alimoothedj1@gmail.com>
2025-12-30 01:52:32 +0300

    Update moo_chapatis.py

--- moo_chapatis.py
+++ moo_chapatis.py
@@ -275,7 +275,12 @@ if __name__ == '__main__':
     if not os.path.exists('static'):
         os.makedirs('static')
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, debug=True, host='0.0.0.0', port=port)
+    try:
+        socketio.run(app, debug=True, host='127.0.0.1', port=port)
+    except Exception as e:
+        print("Error starting app:", e)
+
+
 
 
 
